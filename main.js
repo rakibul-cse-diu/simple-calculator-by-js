dc99819eab3ffbbd563d12f1f8c03c469baf8b7c
rakibul-cse-diu <rakibul2180@gmail.com>
2022-03-02 20:52:23 +0600

    add listener on tergeted button

--- main.js
+++ main.js
@@ -6,10 +6,16 @@ const calBtn = document.querySelectorAll(".cal-btn")
 console.log(calBtn)
 
 calBtn.forEach(btn => {
-    let array = [btn.innerText];
-    if (array.includes("C")) {
-        console.log("paisi")
-    } else {
-        console.log("nah");
-    }
+
+    btn.addEventListener("click", function (e) {
+        console.log(e.target.innerText)
+        dispalyNumber.innerText = dispalyNumber.innerText + e.target.innerText;
+    })
+
+    // let array = [btn.innerText];
+    // if (array.includes("C")) {
+    //     console.log("paisi")
+    // } else {
+    //     console.log("nah");
+    // }
 })
